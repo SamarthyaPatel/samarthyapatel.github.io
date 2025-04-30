f31c87f0cd4bfbe6557e08c2115a4e010aee57aa
Samarthya Patel <86912122+SamarthyaPatel@users.noreply.github.com>
2025-04-30 16:45:50 +0530

    Add files via upload

--- education.js
+++ education.js
@@ -1,14 +1,36 @@
+gsap.registerPlugin(ScrollTrigger);
+
 document.addEventListener('DOMContentLoaded', () => {
 
     let mm = gsap.matchMedia();
 
-    mm.add("(orientation: portrait)", () => {
+    // mm.add("(orientation: portrait)", () => {
 
-        const carousel = document.querySelector(".education-content");
+    //     const carousel = document.querySelector(".education-content");
         
-        gsap.to("#edu-title", { scrollTrigger: {trigger: "#edu-title", start: "top 11%", end: "+=1000", pin: true, pinSpacing: false }  });
-        gsap.to(carousel, {x: "-240vw", scrollTrigger: {trigger: carousel, start: "top 20.5%", end: "+=1000", scrub: .5, pin: true  } });
+    //     gsap.to("#edu-title", { scrollTrigger: {trigger: "#edu-title", start: "top 10%", end: "+=1000", pin: true, pinSpacing: false }  });
+    //     gsap.to(carousel, {x: "-240vw", scrollTrigger: {trigger: carousel, start: "top 20.5%", end: "+=1000", scrub: .5, pin: true  } });
 
-    }); 
+    // }); 
+
+    mm.add("(orientation: portrait)", () => {
+        const carousel = document.querySelector(".education-content");
+
+        console.log(carousel.scrollWidth, window.innerWidth)
+
+        // Pin the whole section instead of title separately
+        gsap.to(carousel, {
+            x: () => `-${(carousel.scrollWidth +50) - window.innerWidth}px`,
+            ease: "none",
+            scrollTrigger: {
+                trigger: "#Education",
+                start: "top 15%",
+                end: () => `+=${carousel.scrollWidth}`,
+                scrub: .5,
+                pin: true,
+                anticipatedPin: 1
+            }
+        });
+    });
 
 });
